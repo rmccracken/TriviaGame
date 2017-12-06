c70334b4d35042b8b82e414cf02ea0b4864a86bd
rmccracken <30883836+rmccracken@users.noreply.github.com>
2017-12-05 19:17:33 -0500

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,22 +24,15 @@ var getQuestions = function(){
 	questionBuilder("James Bond is a character based on what author's writings?", "Ian Fleming", "Robert Frost", "George orwell", "George R.R. Martin", "1")];
 };
 
-// alert("Welcome to the Quiz!");
-// variable for current question
 var currentQuestion = 0;
-// variable to hold corrent answer count
 var correctAnswers = 0;
-// variable to hold incorrect answer count
 var incorrectAnswers = 0;
-// variable to hold the unanswered questions
 var unanswered = 0;
-// Holds questions, choices, and answers
-var questions = getQuestions();
 
+var questions = getQuestions();
 var totalQuestions = questions.length;
-// sets timer to  20 
+
 var timer = 120;
-// variable that will  contdown timer
 var timerCount;
 // hides divs for later
 var divUpdate = function(){
@@ -48,11 +41,11 @@ var divUpdate = function(){
 	$("#incorrectAnswers").hide();
 	$("#unAnswered").hide();
 }
-// sets up countdown function to go down by one second
+// sets up countdown 
 function run() {
 	timerCount = setInterval(countdown, 1000);
 }
-// actual countdown function
+
 function countdown(){
 	timer--;
 
@@ -79,13 +72,6 @@ var choice3 = document.getElementById('choice3');
 var choice4 = document.getElementById('choice4');
 var nextButton = document.getElementById('nextButton');
 
-// var container = $("#container");
-// var questionView = $("#question");
-// var choice1 = $("#choice1");
-// var choice2 = $("#choice2");
-// var choice3 = $("#choice3");
-// var choice4 = $("#choice4");
-
 // function to pull info from questions var
 function loadQuestion(questionIndex){
 	var i = questions[questionIndex];
@@ -98,20 +84,20 @@ function loadQuestion(questionIndex){
 // used to move to next question
 var nextQuestion = function(){
 	var selectedChoice = $("input[type=radio]:checked");
-	// does each radio button = its selected choice
+
 	console.log(selectedChoice);
 	var userAnswer = selectedChoice.val();
 		selectedChoice.prop('checked', false);
-	// adds +1 to undefined answers
+
 	if (userAnswer == undefined){
 		unanswered++;
 	}
-	// adds +1 to right answers
+
 	else {
 		if (questions[currentQuestion].answer == userAnswer){
 		correctAnswers++;
 		}
-		// does whats left the incorrect answers
+		
 		else {
 			incorrectAnswers++}
 		}
@@ -120,11 +106,11 @@ var nextQuestion = function(){
 	if (currentQuestion == totalQuestions - 1){
 		submit.textContent =  "submit";
 	}
-	// we are done show results
+
 	if(currentQuestion == totalQuestions){
 		showSummary();
 	}
-	// loads another question
+
 	else {
 		loadQuestion(currentQuestion);
 	}
